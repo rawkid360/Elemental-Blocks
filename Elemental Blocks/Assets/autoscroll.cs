9d1f4405f283c517a672371e211bcf4d3879d671
Yassin Chadli <yassinchadli@hotmail.co.uk>
2019-12-11 21:29:36 +0000

    Auto-Scroll Complete
    Backup

---
+++ Elemental Blocks/Assets/autoscroll.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class autoscroll : MonoBehaviour
+{ public GameObject Player;
+
+
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Player==null)
+        {
+            Player = GameObject.FindGameObjectWithTag("Player");
+        }
+        transform.position = Player.transform.position + offset;  
+    }
+}
