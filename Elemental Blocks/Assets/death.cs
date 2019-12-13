83ab71c951f87cc057bfaeef43741e71b8828f13
Yassin Chadli <yassinchadli@hotmail.co.uk>
2019-12-13 02:59:56 +0000

    Earth Mechanic Added
    Earth ability done

--- Elemental Blocks/Assets/death.cs
+++ Elemental Blocks/Assets/death.cs
@@ -4,23 +4,42 @@ using UnityEngine;
 
 public class death : MonoBehaviour
 {
+public float lives;
     // Start is called before the first frame update
     void Start()
     {
-        
+        lives = 1.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-    
+    if(lives >= 2)
+            {
+             lives = 2;
+            }
     }
          
    private void OnTriggerEnter(Collider other)
    {
-   if(other.gameObject.tag == "spike")
+      if(other.gameObject.tag == "spike")
+      {
+            lives -= 1;
+            if(lives <= 0)
+            {
+              Destroy(gameObject);
+            }
+      }
+        if(other.gameObject.tag == "earth")
         {
-            Destroy(gameObject);
+            lives += 1;
+        }
+   }
+   private void OnTriggerStay(Collider other)
+   {
+   if(other.gameObject.tag == "earth")
+        {
+            lives += 1;
         }
    }
 }
--- Elemental Blocks/Assets/earth.cs
+++ Elemental Blocks/Assets/earth.cs
@@ -13,6 +13,12 @@ public class earth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+  
     }
+    private void OnTriggerEnter(Collider other) {
+     if (other.gameObject.tag == "Player")
+     {
+     Destroy(gameObject);
+     }
+     }
 }
