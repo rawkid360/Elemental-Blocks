6e855557b1e4260b08a950be27f21e87a1dec726
Yassin Chadli <yassinchadli@hotmail.co.uk>
2019-12-13 01:20:08 +0000

    Wind Mechanic Added
    Wind Ability Complete

--- Elemental Blocks/Assets/wind.cs
+++ Elemental Blocks/Assets/wind.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class wind : MonoBehaviour
 {
+public GameObject Player1;
+public Vector2 jumpHeight;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,16 @@ public class wind : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+      if (Player1==null)
+        {
+            Player1 = GameObject.FindGameObjectWithTag("Player");
+        } 
     }
+     private void OnTriggerEnter(Collider other)
+     {
+     if (other.gameObject.tag == "Player")
+     {
+     Player1.GetComponent<Rigidbody>().AddForce(jumpHeight, ForceMode.Impulse);
+     }
+     }
 }
