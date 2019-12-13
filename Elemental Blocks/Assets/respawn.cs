a1ea061604319524bd80aa40ee7b702f4a15f59e
Yassin Chadli <yassinchadli@hotmail.co.uk>
2019-12-13 00:49:04 +0000

    Respawn Script Complete
    Allows the Player character to respawn

---
+++ Elemental Blocks/Assets/respawn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawn : MonoBehaviour {
+
+
+    public GameObject Playerprefab;
+    public GameObject Player;
+    public float Timer;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        
+        Spawn();
+    }
+
+    void Spawn()
+    {
+        Timer = 2;
+        Player = (GameObject)Instantiate(Playerprefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Player == null)
+        {
+            Timer -= Time.deltaTime;
+            if (Timer <= 0)
+            {
+                Spawn();
+
+            }
+        }
+    }
+}
